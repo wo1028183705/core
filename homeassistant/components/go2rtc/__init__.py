8d6afa9cd7adb588d3fe027bac501e4563251aac
Erik <erik@montnemery.com>
2024-10-30 14:01:10 +0100

    Add watchdog to monitor and respawn go2rtc server

--- homeassistant/components/go2rtc/__init__.py
+++ homeassistant/components/go2rtc/__init__.py
@@ -37,7 +37,7 @@ from homeassistant.helpers.typing import ConfigType
 from homeassistant.util.hass_dict import HassKey
 from homeassistant.util.package import is_docker_env
 
-from .const import DOMAIN
+from .const import DEFAULT_URL, DOMAIN
 from .server import Server
 
 _LOGGER = logging.getLogger(__name__)
@@ -112,7 +112,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
         hass.bus.async_listen(EVENT_HOMEASSISTANT_STOP, on_stop)
 
-        url = "http://localhost:1984/"
+        url = DEFAULT_URL
 
     hass.data[_DATA_GO2RTC] = url
     discovery_flow.async_create_flow(
--- homeassistant/components/go2rtc/const.py
+++ homeassistant/components/go2rtc/const.py
@@ -3,3 +3,4 @@
 DOMAIN = "go2rtc"
 
 CONF_BINARY = "binary"
+DEFAULT_URL = "http://localhost:1984/"
